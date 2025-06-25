f2f8135903c5b358cc9ce9e33afebc0803eb3809
Nandini Jaiswal <159615999+NandiniJaiswal05@users.noreply.github.com>
2025-06-25 19:59:07 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+import streamlit as st
+import os
+import requests
+import torch
+
+MODEL_URL = "https://drive.google.com/file/d/13RiUDLFkhGtO6g1KDS0bdcCMHlSDeY_g/view?usp=sharing"
+MODEL_PATH = "checkpoints.pth"
+
+@st.cache_resource
+def load_model():
+    if not os.path.exists(MODEL_PATH):
+        st.write("Downloading model...")
+        response = requests.get(MODEL_URL)
+        with open(MODEL_PATH, "wb") as f:
+            f.write(response.content)
+
+    model = torch.load(MODEL_PATH, map_location=torch.device('cpu'))
+    model.eval()
+    return model
+
+model = load_model()
+
+st.title("PyTorch Checkpoint Inference App")
+st.write("Your model is loaded!")
